14c7de215323184d7436a4777006d88894af0bbc
Bradley Guthrie <guppy96@gmail.com>
2018-10-23 21:10:03 -0500

    broken link corrected and updated

--- shoppinglist/project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppinglist/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe( 'Test Recipe', 'test details', 'https://upload.wikimedia.org%2Fwikipedia%2Fcommons%2F3%2F39%2FRecipe.jpg&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ARecipe.jpg&docid=FCKyUIOU_PHYFM&tbnid=z09hcQfR0Jt4dM%3A&vet=10ahUKEwizqKLl_p3eAhWnVN8KHYteCaIQMwhsKAEwAQ..i&w=800&h=672&safe=off&bih=720&biw=1789&q=recipe&ved=0ahUKEwizqKLl_p3eAhWnVN8KHYteCaIQMwhsKAEwAQ&iact=mrc&uact=8')
+    new Recipe( 'Test Recipe', 'test details', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0VwjfPX9Toc_99nnw6GLeGDrtH9Y4UHYfJ9yTut5Gw_EeKdGE_g')
   ];
 
   constructor() { }
