f5971e2b8022f18cb7f39fbed15ed961c880c105
Bradley Guthrie <guppy96@gmail.com>
2018-10-23 19:32:35 -0500

    added recipe model

---
+++ shoppinglist/project/src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { THROW_IF_NOT_FOUND } from "@angular/core/src/di/injector";
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
